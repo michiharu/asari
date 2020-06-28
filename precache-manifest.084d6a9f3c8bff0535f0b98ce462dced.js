c3c9e565226d4844835c240c8a6a9a2e607ce20f
michiharu <michi1diver@gmail.com>
2020-06-29 00:02:27 +0900

    Updates

---
+++ precache-manifest.084d6a9f3c8bff0535f0b98ce462dced.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa128cb2a1e4833bca9e73d789ade7b6",
+    "url": "/asari/index.html"
+  },
+  {
+    "revision": "2d3fbd89ee38067eec6c",
+    "url": "/asari/static/js/2.64bc9394.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/asari/static/js/2.64bc9394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f068c07f7581fd39fcf",
+    "url": "/asari/static/js/main.9f03e216.chunk.js"
+  },
+  {
+    "revision": "c1ab432fa80bb33dbdf3",
+    "url": "/asari/static/js/runtime-main.475522b0.js"
+  }
+]);
